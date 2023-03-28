b2d139ec6d0d26b979275d3f6dd3379dfab5c9d1
Serhii <shram.serhiy@gmail.com>
2023-03-28 20:30:28 +0300

    Add console args
    Signed-off-by: Serhii <shram.serhiy@gmail.com>

--- Sources/main.cpp
+++ Sources/main.cpp
@@ -2,9 +2,16 @@
 #include "pch.hpp"
 #include "simulator.hpp"
 
-int main() {
+int main(int argc, char *argv[]) {
+  if (argc < 2) {
+    std::cout << "Usage: " << argv[0] << " <filename.config>\n";
+    return 1;
+  }
+
+  std::string config_filename(argv[1]);
+
   try {
-    sprsim::config config("test.config");
+    sprsim::config config(config_filename);
     sprsim::city_map map(config.get<std::string>("map_filename"));
     std::unique_ptr<sprsim::Idisplay> display =
         std::make_unique<sprsim::console_dislpay>();
